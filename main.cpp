748e6f4f6300729123b6280238e1d92b9a5ae122
SC20K <tyktuk.gogo@mail.ru>
2024-12-25 00:10:07 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <sdt.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Введите два числа: ";
+    cin >> a >> b;
+    cout << "Сумма: " << (a + b) << endl;
+    cout << "Разность: " << (a - b) << endl;
+    return 0;
+}
