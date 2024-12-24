60d31bd72c45d2d68b4b35b4fb7f0ff6d99cbc55
SC20K <tyktuk.gogo@mail.ru>
2024-12-25 00:15:05 +0300

    lab5.cpp updated

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
     int a, b;
     cout << "Введите два числа: ";
     cin >> a >> b;
-    cout << "Сумма: " << (a + b) << endl;
+    cout << "Вот Сумма: " << (a + b) << endl;
     cout << "Разность: " << (a - b) << endl;
     return 0;
 }
